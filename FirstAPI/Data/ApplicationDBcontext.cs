9b7b69c646c084426bc3f81680f36bec90e6b16d
NEBULA\Asiphile <Asiphile@nebula.co.za>
2025-02-14 13:28:59 +0200

    Added DB context

--- FirstAPI/Data/ApplicationDBcontext.cs
+++ FirstAPI/Data/ApplicationDBcontext.cs
@@ -1,11 +1,13 @@
 ﻿using FirstAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 /// <summary>
 /// Represents the application's database context, managing entity configurations and database operations.
-/// Inherits from <see cref="DbContext"/> to enable Entity Framework Core functionality.
+/// Inherits from <see cref="IdentityDbContext"/> to enable Entity Framework Core functionality.
 /// </summary>
-public class ApplicationDBContext : DbContext
+public class ApplicationDBContext : IdentityDbContext<User>
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="ApplicationDBContext"/> class.
@@ -16,11 +18,28 @@ public class ApplicationDBContext : DbContext
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole{
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                },
+                   new IdentityRole{
+                    Name = "User",
+                    NormalizedName = "USER",
+                }
+            };
+        builder.Entity<IdentityRole>().HasData(roles);
+    }
+
     /// <summary>
     /// DbSet properties represent tables in the database.
     /// Add entities as needed.
     /// </summary>
-     public DbSet<Achivement> achivements { get; set; }
+    public DbSet<Achivement> achivements { get; set; }
      public DbSet<Education> educations { get; set; }
      public DbSet<Experience> experiences { get; set; }
      public DbSet<Hobby> hobbies { get; set; }
---
+++ FirstAPI/Migrations/20250211122115_IntitalCreate.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FirstAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntitalCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "achivements",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateAquired = table.Column<DateOnly>(type: "date", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_achivements", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "educations",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Degree = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Institution = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EndDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Grade = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageSrc = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_educations", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "experiences",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Organisation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Experiences = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageSrc = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_experiences", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "hobbies",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageSrc = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_hobbies", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "projects",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Link = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Tags = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageSrc = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_projects", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "achivements");
+
+            migrationBuilder.DropTable(
+                name: "educations");
+
+            migrationBuilder.DropTable(
+                name: "experiences");
+
+            migrationBuilder.DropTable(
+                name: "hobbies");
+
+            migrationBuilder.DropTable(
+                name: "projects");
+        }
+    }
+}
